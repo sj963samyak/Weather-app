b635896b420ecda9125e1e306d3055086bada11e
sj963samyak <100397887+sj963samyak@users.noreply.github.com>
2023-08-17 23:15:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,15 +13,15 @@ async function checkWeather(city) {
   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
   document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
   if (data.weather[0].main == "Clouds") {
-    weatherIcon.src = "/Weather app/images/clouds.png";
+    weatherIcon.src = "Weather app/images/clouds.png";
   } else if (data.weather[0].main == "Clear") {
-    weatherIcon.src = "/Weather app/images/clear.png";
+    weatherIcon.src = "Weather app/images/clear.png";
   } else if (data.weather[0].main == "Rain") {
-    weatherIcon.src = "/Weather app/images/rain.png";
+    weatherIcon.src = "Weather app/images/rain.png";
   } else if (data.weather[0].main == "Drizzle") {
-    weatherIcon.src = "/Weather app/images/drizzle.png";
+    weatherIcon.src = "Weather app/images/drizzle.png";
   } else if (data.weather[0].main == "Mist") {
-    weatherIcon.src = "/Weather app/images/mist.png";
+    weatherIcon.src = "Weather app/images/mist.png";
   }
   document.querySelector(".weather").style.display = "block";
 }
